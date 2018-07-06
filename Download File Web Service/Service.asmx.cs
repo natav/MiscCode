b85423c0dadb3f50024de4ecb0582fe52aa8cc92
Natalya Varshavskaya <natalyav@granicus.com>
2018-07-06 14:54:57 -0500

    add 2 solutions: download File Web Service and Download File through URL console app

---
+++ Download File Web Service/Service.asmx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Web.Services;
+
+namespace WebService1
+{
+    /// <summary>
+    /// Summary description for Service1
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    public class Service1 : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string TestMethod()
+        {
+            return "OK";
+        }
+
+        [WebMethod]
+        public string DownloadFile(string remoteUri, string fileNameToSave)
+        { // sample URL: http://legistar-us-staging-web-1.granicusops.com/files/InSite/Files/DFLD/Attachments/03163048-dfd2-4b21-97c0-bcd0856811c6.pdf
+            try
+            {
+                // Create a new WebClient instance
+                System.Net.WebClient myWebClient = new System.Net.WebClient();
+
+                Debug.WriteLine("Downloading " + remoteUri);
+
+                //// Download the Web resource and save it into a data buffer
+                //byte[] myDataBuffer = myWebClient.DownloadData(remoteUri);
+                
+                // Write to File
+                //File.WriteAllBytes("c:/Temp/" + fileNameToSave, myDataBuffer);
+               
+                myWebClient.DownloadFile(remoteUri, "c:/Temp/" + fileNameToSave);
+               
+                Debug.WriteLine("Download successful, file created.");
+            }
+            catch (Exception ex)
+            {
+                return "Failed to download file." + ex.Message;
+            }
+            return "File successfully downloaded.";
+        }
+    }
+}
---
+++ DownloadFileThroughURI - console app/DownloadFileThroughURI/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\nEnter URL: ");// sample: http://legistar-us-staging-web-1.granicusops.com/files/InSite/Files/DFLD/Attachments/03163048-dfd2-4b21-97c0-bcd0856811c6.pdf
+            string remoteUri = Console.ReadLine();
+
+            // Create a new WebClient instance
+            System.Net.WebClient myWebClient = new System.Net.WebClient();
+
+            // Download home page data
+            Console.WriteLine("Downloading " + remoteUri);
+
+            // Download the Web resource and save it into a data buffer
+            byte[] myDataBuffer = myWebClient.DownloadData(remoteUri);
+
+            // Write to File (need to know the ext.)
+            File.WriteAllBytes("c:/Temp/1.tif", myDataBuffer);
+
+            //// Display the downloaded data
+            //string download = Encoding.ASCII.GetString(myDataBuffer);
+
+            //Console.WriteLine(download);
+
+            Console.WriteLine("Download successful.");
+
+            Console.Read();
+        }
+    }
+}
