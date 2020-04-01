ddb89aa9fcdb75372e1e361644465c5e33eb82a4
Natalya Varshavskaya <natalyav@granicus.com>
2020-04-01 16:49:13 -0500

    add Swagger package and code in Startup to generate API docs.

--- MeetingSkillAPI/Startup.cs
+++ MeetingSkillAPI/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -8,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.OpenApi.Models;
 
 namespace MeetingSkillAPI
 {
@@ -24,6 +28,27 @@ namespace MeetingSkillAPI
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { 
+                    Title = "Meeting Skill API Docs",
+                    Version = "1",
+                    Description = "Web API Alexa Skill app",
+                    //TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "NV",
+                        Email = "natalyav@granicus.com",
+                        //Url = new Uri("https://xxxx"),
+                    },
+                    //License = new OpenApiLicense
+                    //{
+                    //    Name = "Use under LICX",
+                    //    Url = new Uri("https://example.com/license"),
+                    //}
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +60,16 @@ namespace MeetingSkillAPI
             }
 
             app.UseMvc();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Meeting Skill API V1");
+            });
         }
     }
 }
