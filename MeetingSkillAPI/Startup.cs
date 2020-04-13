7a5ea365f8b2e7e52ab6f71040c33ccc7ed2df9b
Natalya Varshavskaya <natalyav@granicus.com>
2020-04-13 18:20:01 -0500

    add comment for how to call swagger UI for this project.

--- MeetingSkillAPI/Startup.cs
+++ MeetingSkillAPI/Startup.cs
@@ -62,6 +62,7 @@ namespace MeetingSkillAPI
             app.UseMvc();
 
             // Enable middleware to serve generated Swagger as a JSON endpoint.
+            // in browser: http://localhost:65209/swagger/index.html 
             app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
