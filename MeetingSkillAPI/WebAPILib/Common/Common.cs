6cac332d6222bf1fd7008d487ade02042240eaf7
Natalya Varshavskaya <natalyav@granicus.com>
2020-04-13 18:20:46 -0500

    Build response string based on the POST response.

--- MeetingSkillAPI/WebAPILib/Common/Common.cs
+++ MeetingSkillAPI/WebAPILib/Common/Common.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MeetingSkillAPI.WebAPILib.Common
@@ -14,5 +15,29 @@ namespace MeetingSkillAPI.WebAPILib.Common
             outObject.error = outMessage;
             return outObject;
         }
+
+        public static string MeetingsListingResultString(Meetings results)
+        {
+            if (results.meetings.Length == 0)
+                return "No upcoming meetings found for the {city}.";
+
+            StringBuilder response = new StringBuilder();
+
+            response.Append($"Listing {results.meetings.Length.ToString()} meetings:");
+            int counter = 1;
+
+            foreach (Meeting meetingRecord in results.meetings)
+            {
+                response.Append($"{counter}: {meetingRecord.EventBodyName}'s meeting is scheduled for the {meetingRecord.EventDate} at {meetingRecord.EventTime}");
+
+                if (!string.IsNullOrEmpty(meetingRecord.EventLocation))
+                    response.Append($" in {meetingRecord.EventLocation}  ");
+
+                counter++;
+            }
+
+            return response.ToString();
+        }
+       
     }
 }
