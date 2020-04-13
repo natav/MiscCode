a7fc11506024b7a0a79303d1ce1c0f40d5ed0abc
Natalya Varshavskaya <natalyav@granicus.com>
2020-04-13 18:22:00 -0500

    Add properties and build URL code.

--- MeetingSkillAPI/WebAPILib/WebAPILib.cs
+++ MeetingSkillAPI/WebAPILib/WebAPILib.cs
@@ -12,48 +12,53 @@ namespace MeetingSkillAPI.WebAPILib
 {
     public class WebAPILib
     {
+        public string BaseURL { get; set; }
+        public string ClientName { get; set; }
+
         public WebAPILib()
         {
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Ssl3 | SecurityProtocolType.Tls | SecurityProtocolType.Tls11 | SecurityProtocolType.Tls12;
-        }
+            
+            var clientName = "jason";
 
-        public string GetURL(string table, string method, string id = "", string body = "", string searchString = "", string field = "", bool addJsonDecorator = true)
-        {
-            string baseUrl = string.Format("https://{0}/ewws/{1}{2}", Host, method, addJsonDecorator == true ? "/.json" : "");
-
-            // Order is important
-            var urlArgs = new List<string>()
+            switch (ClientName) 
             {
-              "$KB=" + KBName,
-              "$table=" + table,
-              "$login=" + User,
-              "$password=" + Password,
-              "$lang=" + Lang,
-              id != "" ? "id=" + id : "",
-              body,
-              searchString,
-              field,
-              method == "EWDelete" ? "deleteRule=APPLY_DELETE_WHERE_POSSIBLE" : ""
-            };
-
-            // Remove the empty args
-            urlArgs.RemoveAll(str => String.IsNullOrEmpty(str));
-
-            string Url = String.Format("{0}?{1}", baseUrl, string.Join("&", urlArgs));
-
-            return Url;
+                case "Milwaukee":
+                    clientName = "milwaukee";
+                    break;
+                case "Ann Arbor":
+                    clientName = "a2gov";
+                    break;
+            }
+            BaseURL = $"https://webapi.legistar.com/v1/{clientName}/events";
         }
 
-        public Meetings GetEventsStartingFromDate(string hostName, string eventStartDate, string bodyId = "", int numberOfRecordsReturned = 2)
+        private string GetURL(DateTime eventStartDate, string bodyId, int numberOfRecordsReturned)
+        {
+            // sample: https://webapi.legistar.com/v1/jason/Events?$filter=EventDate+ge+datetime%272017-09-01%27&$top=2
+            // sample: https://webapi.legistar.com/v1/jason/Events?$filter=EventDate+ge+datetime%272019-03-08%27+and+EventBodyId+eq+139&$top=2
+            // different endpoint: https://webapi.legistar.com/v1/jason/EventDates/138?FutureDatesOnly=true&$top=2
+
+            // sample in dev: https://webapi.dev.legistar.com/v1/a2gov/Events?$filter=EventDate+ge+datetime%272017-09-01%27&$top=10
+            // sample in dev: https://webapi.dev.legistar.com/v1/a2gov/Events?$filter=EventDate+ge+datetime%272017-09-01%27&$top=10&$orderby=EventBodyId
+
+            string formattedDate = eventStartDate.ToString("yyyy-mm-dd");
+
+            if (bodyId == "")
+            {
+                return $"{BaseURL}?$filter=EventDate+ge+datetime%27{formattedDate}%27&$top={numberOfRecordsReturned}&$orderby=EventBodyId";
+            }
+            else
+            {
+                return $"{BaseURL}?$filter=EventDate+ge+datetime%27{formattedDate}%27+and+EventBodyId+eq+{bodyId}&$top={numberOfRecordsReturned}";
+            }
+        }
+
+        public Meetings GetEventsStartingFromDate(DateTime eventStartDate, string bodyId = "", int numberOfRecordsReturned = 2)
         {
             using (HttpClient client = new HttpClient())
             {
-                string Url = GetURL(table, "EWRead", id);
-                // sample: https://webapi.legistar.com/v1/jason/Events?$filter=EventDate+ge+datetime%272017-09-01%27&$top=2
-                // sample: https://webapi.legistar.com/v1/jason/Events?$filter=EventDate+ge+datetime%272019-03-08%27+and+EventBodyId+eq+139&$top=2
-                // sample: https://webapi.legistar.com/v1/jason/EventDates/138?FutureDatesOnly=true&$top=2
-                // sample: https://webapi.dev.legistar.com/v1/a2gov/Events?$filter=EventDate+ge+datetime%272017-09-01%27&$top=10
-                // sample: https://webapi.dev.legistar.com/v1/a2gov/Events?$filter=EventDate+ge+datetime%272017-09-01%27&$top=200&$orderby=EventBodyId
+                string Url = GetURL(eventStartDate, bodyId, numberOfRecordsReturned);
 
                 client.BaseAddress = new Uri(Url);
 
