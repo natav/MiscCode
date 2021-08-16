b5db5c2afef94e2d8185bbc88b30a5c1e00d9c75
Natalya Varshavskaya <natalyav@github.com>
2021-08-16 14:06:29 -0500

    Aspose converter utility changes to make it more user friendly.

--- AsposePDF/AsposePDF/Program.cs
+++ AsposePDF/AsposePDF/Program.cs
@@ -1,9 +1,6 @@
 ﻿using Aspose.Pdf;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Aspose.PDF
 {
@@ -12,45 +9,70 @@ namespace Aspose.PDF
         static void Main(string[] args)
         {
                 try
-                {
+            {
 
                 Aspose.Pdf.License license = new Aspose.Pdf.License();
                 // Set license
                 license.SetLicense("Aspose.Pdf.lic");
 
-                Console.WriteLine("License set successfully.");
+                //Console.WriteLine("License set successfully.");
+
+                Console.WriteLine("Type in the full path for pdf report you would like to convert:");
 
                 // Load source PDF file
-                Document doc = new Document("D:\\Aspose\\Input\\Aspose_input.pdf");
+                //Document doc = new Document("D:\\Aspose\\Input\\Aspose_input.pdf");
+                var pdfFile = Console.ReadLine().Trim();
 
-                    // Instantiate HTML Save options object
-                    HtmlSaveOptions newOptions = new HtmlSaveOptions();
-                    newOptions.PartsEmbeddingMode = HtmlSaveOptions.PartsEmbeddingModes.EmbedAllIntoHtml;
-                    newOptions.RemoveEmptyAreasOnTopAndBottom = true;
-                    newOptions.SplitIntoPages = false;
-                    newOptions.HtmlMarkupGenerationMode = HtmlSaveOptions.HtmlMarkupGenerationModes.WriteAllHtml;
-                    newOptions.FixedLayout = true;
-
-                    // This is just optimization for IE and can be omitted 
-                    newOptions.LettersPositioningMethod = HtmlSaveOptions.LettersPositioningMethods.UseEmUnitsAndCompensationOfRoundingErrorsInCss;
-                    newOptions.RasterImagesSavingMode = HtmlSaveOptions.RasterImagesSavingModes.AsEmbeddedPartsOfPngPageBackground;
-                    newOptions.FontSavingMode = HtmlSaveOptions.FontSavingModes.SaveInAllFormats;
-
-                    // Output file path 
-                    string outHtmlFile = "D:\\Aspose\\Output\\Aspose_licensed_output_with_RemoveEmptyAreasOnTopAndBottom_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".html";
-                    
-                    doc.Save(outHtmlFile, newOptions);
-
-                    Console.WriteLine("PDF converted to HTML successfully.");
-                    Console.Read();
+                if (!FileExists(pdfFile))
+                {
+                    Console.WriteLine("File doesn't exist! Exiting...");
+                    System.Threading.Thread.Sleep(5000);
+                    Environment.Exit(0);
                 }
-                catch (Exception ex)
+                else if (Path.GetExtension(pdfFile).ToUpper() != ".PDF")
+                {
+                    Console.WriteLine("File isn't pdf! Exiting...");
+                    System.Threading.Thread.Sleep(5000);
+                    Environment.Exit(0);
+                }
+
+               
+                Console.WriteLine($"{Environment.NewLine}Converting...{Environment.NewLine}");
+
+                Document doc = new Document(pdfFile);
+
+                // Instantiate HTML Save options object
+                HtmlSaveOptions newOptions = new HtmlSaveOptions();
+                newOptions.PartsEmbeddingMode = HtmlSaveOptions.PartsEmbeddingModes.EmbedAllIntoHtml;
+                newOptions.RemoveEmptyAreasOnTopAndBottom = true;
+                newOptions.SplitIntoPages = false;
+                newOptions.HtmlMarkupGenerationMode = HtmlSaveOptions.HtmlMarkupGenerationModes.WriteAllHtml;
+                newOptions.FixedLayout = true;
+
+                // This is just optimization for IE and can be omitted 
+                newOptions.LettersPositioningMethod = HtmlSaveOptions.LettersPositioningMethods.UseEmUnitsAndCompensationOfRoundingErrorsInCss;
+                newOptions.RasterImagesSavingMode = HtmlSaveOptions.RasterImagesSavingModes.AsEmbeddedPartsOfPngPageBackground;
+                newOptions.FontSavingMode = HtmlSaveOptions.FontSavingModes.SaveInAllFormats;
+
+                // Output file path 
+                string outHtmlFile = Path.Combine(Directory.GetCurrentDirectory(), "Converted_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".html");
+
+                doc.Save(outHtmlFile, newOptions);
+
+                Console.WriteLine($"PDF converted to HTML successfully:{Environment.NewLine} {outHtmlFile}");
+                Console.Read();
+            }
+            catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message);
                     Console.Read();
-
                 }
           
+        }
+
+        private static bool FileExists(string pdfFile)
+        {
+            return File.Exists(pdfFile) ? true : false;
         }
     }
 }
