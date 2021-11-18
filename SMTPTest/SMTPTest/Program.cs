4381f067e6b374c40104982db4020469f135df36
natalya <nvarshavskaya@yahoo.com>
2021-11-17 18:12:23 -0600

    Make it generic: read address, port and server from arguments

--- SMTPTest/SMTPTest/Program.cs
+++ SMTPTest/SMTPTest/Program.cs
@@ -1,10 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
-
-using System.Net.Mail;
 
 namespace SMTPTest
 {
@@ -14,28 +13,61 @@ namespace SMTPTest
         {
             try
             {
+                string smtpServer = "";
+                string smtpPort = "25";
+                string toAddress = "";
+
+                for (int i = 0; i < args.Length; i++)
+                {
+                    string[] param = args[i].Split(':');
+
+                    switch (param[0])
+                    {
+                        case "smtpServer":
+                            smtpServer = param[1].ToLower();
+                            break;
+                        case "smtpPort":
+                            smtpPort = param[1].ToLower();
+                            break;
+                        case "toAddress":
+                            toAddress = param[1];
+                            break;
+                        default:
+                            break;
+                    }
+                }
+
+                if (args.Length != 3)
+                {
+                    Console.WriteLine("Please provide arguments.");
+                    Console.WriteLine("SMTPTest.exe smtpServer:\"SMTP Server Address\" smtpPort:\"SMTP Server Port\" toAddress:\"Address to whom send test email\"");
+                    Console.WriteLine("Press ENTER to continue...");
+                    Console.ReadLine();
+                    return;
+                }
+
                 MailMessage message = new MailMessage();
-                message.From = new MailAddress("natalya.varshavskaya@granicus.com"); // ("natalya.varshavskaya@granicus.com");
+                message.From = new MailAddress(toAddress);
 
-                message.To.Add("natalya.varshavskaya@granicus.com"); // ("natalya.varshavskaya@granicus.com");
+                message.To.Add(toAddress);
 
-                message.Subject = "my subject - Test";
-                message.Body = "mail body";
-                string MailServer = "smtp.granicuslabs.com"; //"10.3.5.24"; //"smtp.dev.granicus.com"; //"smtp.granicuslabs.com"; // "localhost";
+                message.Subject = "SMTP Test";
+                message.Body = "eMail Body";
+                string MailServer = smtpServer;
 
                 SmtpClient emailClient = new SmtpClient(MailServer);
-                emailClient.Port = 587; // 25; //587; // 25;
+                emailClient.Port = int.Parse(smtpPort);
                 emailClient.DeliveryMethod = SmtpDeliveryMethod.Network;
                 emailClient.EnableSsl = false;
 
                 emailClient.Send(message);
 
-                Console.WriteLine("SMTP Server: " + MailServer + "; Port: " + emailClient.Port+ " Sent to: " + message.To + ". Sent From: " + message.From + ". Done!");
+                Console.WriteLine($"SMTP Server: {smtpServer}{Environment.NewLine}Port: {smtpPort}{Environment.NewLine}Sent To: {toAddress}{Environment.NewLine}Sent From: {toAddress}{Environment.NewLine}Sent!");
                 Console.ReadLine();
             }
            catch (Exception ex)
             {
-                Console.WriteLine("An Error Occured" + ex.InnerException + " " + ex.Message);
+                Console.WriteLine($"An Error Occured:{Environment.NewLine}{ex.InnerException}{Environment.NewLine}{Environment.NewLine}Error Message:{Environment.NewLine}{ex.Message}");
                 Console.ReadLine();
             }
         }
